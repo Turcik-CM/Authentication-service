f0a467de4412540473d180732406b118743d369e
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-09-19 18:39:36 +0500

    migration fixsed

--- migrations/000001_auth_service_table.up.sql
+++ migrations/000001_auth_service_table.up.sql
@@ -1,36 +1,38 @@
 CREATE TYPE role AS ENUM ('admin', 'user', 'c-admin');
+CREATE TYPE country AS ENUM('Azerbaijan', 'Kazakhstan', 'Kyrgyzstan', 'Turkey', 'Uzbekistan');
+
+CREATE TABLE IF NOT EXISTS countries
+(
+    id           UUID DEFAULT gen_random_uuid(),
+    city_name    VARCHAR UNIQUE ,
+    country      country,
+    nationality  INTEGER PRIMARY KEY,
+    flag         VARCHAR
+);
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id         UUID PRIMARY KEY         DEFAULT gen_random_uuid(),
-    email      VARCHAR UNIQUE,
-    first_name VARCHAR,
-    last_name  VARCHAR,
-    phone      VARCHAR(13) UNIQUE,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    deleted_at BIGINT                   DEFAULT 0,
-    UNIQUE(email, deleted_at)
-);
-
-CREATE TABLE IF NOT EXISTS user_profile
-(
-    user_id       UUID REFERENCES users (id) ON DELETE CASCADE,
-    username      VARCHAR UNIQUE,
-    nationality   VARCHAR,
-    bio           VARCHAR,
-    role          role                     DEFAULT 'user',
-    profile_image VARCHAR                  DEFAULT 'no image',
-    created_at    TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    updated_at    TIMESTAMP WITH TIME ZONE DEFAULT now(),
-    is_active     BOOLEAN                  DEFAULT TRUE,
-    UNIQUE (username)
+    id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email          VARCHAR UNIQUE,
+    phone          VARCHAR(13) UNIQUE,
+    password       VARCHAR NOT NULL,
+    first_name     VARCHAR NOT NULL ,
+    last_name      VARCHAR NOT NULL ,
+    username       VARCHAR,
+    nationality    VARCHAR REFERENCES countries(nationality),
+    bio            VARCHAR,
+    role           role,
+    profile_image  VARCHAR DEFAULT 'no image',
+    created_at     TIMESTAMP DEFAULT now(),
+    updated_at     TIMESTAMP DEFAULT now(),
+    deleted_at     BIGINT DEFAULT 0,
+    UNIQUE(username, deleted_at)
 );
 
 CREATE TABLE IF NOT EXISTS follows
 (
-    followers_id UUID REFERENCES users (id) ON DELETE CASCADE,
-    following_id UUID REFERENCES users (id) ON DELETE CASCADE,
-    followed_at  TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    followers_id  UUID REFERENCES users (id) ON DELETE CASCADE,
+    following_id  UUID REFERENCES users (id) ON DELETE CASCADE,
+    followed_at   TIMESTAMP DEFAULT now(),
     PRIMARY KEY (followers_id, following_id)
 );

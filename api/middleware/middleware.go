f02b237b0d53818c9f4f041d4f6cc5eda72166c1
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-09-27 21:57:22 +0500

    added cors

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -3,15 +3,12 @@ package middleware
 import (
 	"auth-service/pkg/token"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Println("Cors middleware triggered")
 		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")

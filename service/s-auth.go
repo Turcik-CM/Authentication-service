f99a901a9c675745405b3defc0dd0c66d6575d8c
xamidullox <xamidulloxxamidullayev592>
2024-10-02 18:49:34 +0500

    auth

--- service/s-auth.go
+++ service/s-auth.go
@@ -2,10 +2,20 @@ package service
 
 import (
 	pb "auth-service/genproto/user"
+	"auth-service/pkg/hashing"
 	"context"
 )
 
 func (s *AuthService) Register(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+
+	hash, err := hashing.HashPassword(in.Password)
+	if err != nil {
+		s.log.Error("failed to hash password", "error", err)
+		return nil, err
+	}
+
+	in.Password = hash
+
 	res, err := s.st.Register(in)
 	if err != nil {
 		s.log.Error("failed to hash password", "error", err)
@@ -14,7 +24,15 @@ func (s *AuthService) Register(ctx context.Context, in *pb.RegisterRequest) (*pb
 	return res, nil
 }
 
-func (s *AuthService) LoginEmail(ctx context.Context, in *pb.LoginEmailRequest) (*pb.LoginResponse, error) {
+func (s *AuthService) LoginEmail(ctx context.Context, in *pb.LoginEmailRequest) (*pb.LoginResponse1, error) {
+	hash, err := hashing.HashPassword(in.Password)
+	if err != nil {
+		s.log.Error("failed to hash password", "error", err)
+		return nil, err
+	}
+
+	in.Password = hash
+
 	res, err := s.st.LoginEmail(in)
 	if err != nil {
 		s.log.Error("failed to hash password", "error", err)
@@ -23,7 +41,15 @@ func (s *AuthService) LoginEmail(ctx context.Context, in *pb.LoginEmailRequest) 
 	return res, nil
 }
 
-func (s *AuthService) LoginUsername(ctx context.Context, in *pb.LoginUsernameRequest) (*pb.LoginResponse, error) {
+func (s *AuthService) LoginUsername(ctx context.Context, in *pb.LoginUsernameRequest) (*pb.LoginResponse1, error) {
+	hash, err := hashing.HashPassword(in.Password)
+	if err != nil {
+		s.log.Error("failed to hash password", "error", err)
+		return nil, err
+	}
+
+	in.Password = hash
+
 	res, err := s.st.LoginUsername(in)
 	if err != nil {
 		s.log.Error("failed to hash password", "error", err)
@@ -51,6 +77,14 @@ func (s *AuthService) RegisterAdmin(ctx context.Context, in *pb.Message) (*pb.Me
 }
 
 func (s *AuthService) UpdatePassword(ctx context.Context, in *pb.UpdatePasswordReq) (*pb.Message, error) {
+	hash, err := hashing.HashPassword(in.Password)
+	if err != nil {
+		s.log.Error("failed to hash password", "error", err)
+		return nil, err
+	}
+
+	in.Password = hash
+
 	res, err := s.st.UpdatePassword(in)
 	if err != nil {
 		s.log.Error("failed to hash password", "error", err)
